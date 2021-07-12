905b416a57b50857ffd85ea23ef1b8ea41c8c57a
Ryan James Spencer <spencer.ryanjames@gmail.com>
2021-07-12 16:04:40 +1000

    Initial example

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use csv::Reader;
+use serde_bytes::ByteBuf;
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+
+type Record = HashMap<String, ByteBuf>;
+
+const NULL: &'static str = "NULL"; // or whatever.
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Field {
+    Unknown,
+    String,
+    Integer,
+    Float,
+}
+
+pub fn read_file(data: &PathBuf) -> Result<Vec<u8>, Box<dyn Error>> {
+    let file = File::open(data)?;
+    let meta = file.metadata()?;
+    let mut reader = BufReader::new(file);
+    let mut contents = Vec::with_capacity(meta.len().try_into()?);
+    reader.read_to_end(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn read_csv(data: &[u8]) -> Result<Vec<Option<Field>>, Box<dyn Error>> {
+    let mut reader = Reader::from_reader(data);
+    let headers = reader.headers().unwrap().clone().into_byte_record();
+    let mut fields = vec![];
+    for record in reader.byte_records() {
+        let record = record?;
+        let record: Record = record.deserialize(Some(&headers))?;
+        for (_, value) in record.into_iter() {
+            fields.push(parse(&value));
+        }
+    }
+    Ok(fields)
+}
+
+fn parse(bytes: &[u8]) -> Option<Field> {
+    let string = match std::str::from_utf8(bytes) {
+        Ok(v) => v,
+        Err(_) => return Some(Field::Unknown),
+    };
+    if string == NULL {
+        return None;
+    }
+    if string.parse::<i64>().is_ok() {
+        return Some(Field::Integer);
+    };
+    if string.parse::<f64>().is_ok() {
+        return Some(Field::Float);
+    };
+    Some(Field::String)
+}
+
+fn histogram(fields: &[Option<Field>]) -> HashMap<Option<Field>, i64> {
+    fields
+        .into_iter()
+        .cloned()
+        .fold(HashMap::new(), |mut acc, f| {
+            *acc.entry(f).or_default() += 1;
+            acc
+        })
+}
+
+fn go(input: &str) -> Result<(), Box<dyn Error>> {
+    let bytes = read_file(&input.into())?;
+    let fields = read_csv(&bytes)?;
+    println!("{:#?}", histogram(fields.as_slice()));
+    Ok(())
+}
+
+fn main() {
+    go("test.csv").unwrap_or_else(|e| {
+        eprintln!("[csv-count] {}", e);
+        std::process::exit(1);
+    });
+}
